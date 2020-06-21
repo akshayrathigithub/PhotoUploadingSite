192aab4ddf10f1720a643a8ed7bd3d9de4efe927
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-21 15:22:31 +0530

    grid added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,23 +8,20 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
   Array = [
-    { Ind: '', color: "rgba(136, 16, 16, 0.452)"},
-    { Ind: 2, color: "#000121"},
-    { Ind: 3, color: "#eb00a5"},
-    { Ind: 4, color: "#6a4389"},
-    { Ind: 2, color: "#cfcd83"},
-    { Ind: 3, color: "#8ad69f"},
-    { Ind: 4, color: "#177857"},
-    { Ind: 3, color: "#ab4653"},
-    { Ind: 1, color: "#4e67cd"},
-    { Ind: 2, color: "#c63b4e"},
-    { Ind: 3, color: "#c9aea8"},
-    { Ind: 4, color: "#df77b9"},
-    { Ind: 1, color: "#1ea49c"},
-    { Ind: 2, color: "#4a1bac"},
-    { Ind: 1, color: "#319607"},
-    { Ind: 2, color: "#312007"},
-    { Ind: 3, color: "#000"}
+    { Ind: '1/1/6/4', color: "rgba(136, 16, 16, 0.452)"},
+    { Ind: '1/4/6/7', color: "#000121"},
+    { Ind: '1/7/9/11', color: "#eb00a5"},
+    { Ind: '1/11/9/15', color: "#6a4389"},
+    { Ind: '1/15/7/21', color: "#cfcd83"},
+    { Ind: '6/1/14/7', color: "#8ad69f"},
+    { Ind: '9/7/19/12', color: "#177857"},
+    { Ind: '9/12/14/15', color: "#ab4653"},
+    { Ind: '14/12/19/15', color: "#4e67cd"},
+    { Ind: '14/1/19/4', color: "#c63b4e"},
+    { Ind: '14/4/19/7', color: "#c9aea8"},
+    { Ind: '13/15/19/21', color: "#df77b9"},
+    { Ind: '7/15/13/18', color: "#1ea49c"},
+    { Ind: '7/18/13/21', color: "#4a1bac"}
   ];
   photos = ["red", "yellow"];
   constructor() {}
