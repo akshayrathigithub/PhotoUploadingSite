4e3048b3c3a468a031f83180508e41c75bc9924b
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-21 20:49:55 +0530

    flip mode added to grid

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  ViewChildren,
+  AfterViewInit,
+  ElementRef,
+  Renderer2,
+} from "@angular/core";
 
 @Component({
   selector: "app-home",
@@ -6,25 +14,47 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./home.component.css"],
   encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChildren("Picture") linkRefs;
   Array = [
-    { Ind: '1/1/6/4', color: "rgba(136, 16, 16, 0.452)"},
-    { Ind: '1/4/6/7', color: "#000121"},
-    { Ind: '1/7/9/11', color: "#eb00a5"},
-    { Ind: '1/11/9/15', color: "#6a4389"},
-    { Ind: '1/15/7/21', color: "#cfcd83"},
-    { Ind: '6/1/14/7', color: "#8ad69f"},
-    { Ind: '9/7/19/12', color: "#177857"},
-    { Ind: '9/12/14/15', color: "#ab4653"},
-    { Ind: '14/12/19/15', color: "#4e67cd"},
-    { Ind: '14/1/19/4', color: "#c63b4e"},
-    { Ind: '14/4/19/7', color: "#c9aea8"},
-    { Ind: '13/15/19/21', color: "#df77b9"},
-    { Ind: '7/15/13/18', color: "#1ea49c"},
-    { Ind: '7/18/13/21', color: "#4a1bac"}
+    { Ind: "1/1/6/4", photos: ["rgba(136, 16, 16, 0.452)","#7fb411"] },
+    { Ind: "1/4/6/7", photos: ["#000121", "#0023b8"] },
+    { Ind: "1/7/9/11", photos: ["#eb00a5", "#3b8c2a"] },
+    { Ind: "1/11/9/15", photos: ["#6a4389", "#986b53"] },
+    { Ind: "1/15/7/21", photos: ["#cfcd83", "#f50422"] },
+    { Ind: "6/1/14/7", photos: ["#8ad69f", "#983f7a"] },
+    { Ind: "9/7/19/12", photos: ["#177857", "#ea24a3"] },
+    { Ind: "9/12/14/15", photos: ["#ab4653", "#3b8c2a", "#986b53"] },
+    { Ind: "14/12/19/15", photos: ["#4e67cd", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",] },
+    { Ind: "14/1/19/4", photos: ["#c63b4e",  "#983f7a", "#ea24a3",] },
+    { Ind: "14/4/19/7", photos: ["#c9aea8", "#3b8c2a", "#f50422", "#983f7a"] },
+    { Ind: "13/15/19/21", photos: ["#df77b9", "#986b53", "#f50422", "#983f7a", "#ea24a3"] },
+    { Ind: "7/15/13/18", photos: ["#1ea49c", "#f50422", "#983f7a", "#ea24a3"] },
+    { Ind: "7/18/13/21", photos: ["#4a1bac", "#7fb411", "#f50422", "#ea24a3"] }
   ];
-  photos = ["red", "yellow"];
-  constructor() {}
-  
+  constructor(private renderer: Renderer2) {
+  }
+  Repeat(Num: number) {
+    setTimeout(() => {
+      if (Num === 20) {
+        console.log("Completed");
+      } else {
+        this.Repeat(Num + 1);
+        let ind = Math.floor(Math.random() * 10);
+        console.log(ind)
+        this.renderer.setStyle(
+          this.linkRefs._results[ind].nativeElement,
+          "transform",
+          "translateX(-50%)"
+        );
+      }
+    }, 4000);
+  }
   ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.Repeat(0);
+  }
+  getWidth(length: number){
+    return `${length*100}%`
+  }
 }
