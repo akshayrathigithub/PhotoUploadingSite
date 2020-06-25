2e38cfd0b854a98d5e2594f9f4431685991fec08
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-25 19:12:21 +0530

    drag and drop enhanced

--- src/app/components/photo-upload/photo-upload.component.ts
+++ src/app/components/photo-upload/photo-upload.component.ts
@@ -6,24 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-upload.component.css']
 })
 export class PhotoUploadComponent implements OnInit {
-
+  Active: boolean = false
   constructor() { }
 
   ngOnInit(): void {
   }
   onDragStart(e: Event){
-    console.log("drag started")
+    // console.log("drag started")
+    null
   }
   onDrop(e){
+    
     e.preventDefault()
-    console.log(e.dataTransfer.items)
+    console.log("Drop")
   }
   onDragEnd(e: Event){
-    console.log("drag Ended")
+    // console.log("drag Ended")
+    null
   }
   onDragOver(e: Event){
     console.log("dragged")
+    
     e.preventDefault()
     e.stopPropagation()
+  }
+  onDragEnter(e){
+    this.Active = true
+    console.log("drag Entered")
+  }
+  onDragLeave(e){
+    this.Active = false
+    console.log("drag Left")
   }
 }
