2df2621281fc5ca80be195a09f469d8591802bb5
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-26 22:47:46 +0530

    grid added to preview section

--- src/app/components/photo-upload/photo-upload.component.ts
+++ src/app/components/photo-upload/photo-upload.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PhotoUploadComponent implements OnInit {
   Dragged: boolean = false
   Dropped: boolean = false
+  
   constructor() { }
 
   ngOnInit(): void {
