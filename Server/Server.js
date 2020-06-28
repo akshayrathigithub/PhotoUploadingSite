297910250ec40d17eada8ea5e3d848683f840210
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-28 18:23:46 +0530

    Server setup

--- Server/Server.js
+++ Server/Server.js
@@ -1,5 +1,17 @@
-const express = require('express')
+const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const Port = 3000 || process.env
 
-app.listen(Port, ()=>{ console.log("Server is running at "+ Port)})
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  next()
+})
+
+app.listen(Port, () => {
+  console.log("Server is running at " + Port)
+})
