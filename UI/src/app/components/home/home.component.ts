5cba7fee41c9dc2b0d58e12b891b8c3cc3f270e5
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-29 01:19:54 +0530

    translate3d added for better performance

--- UI/src/app/components/home/home.component.ts
+++ UI/src/app/components/home/home.component.ts
@@ -18,10 +18,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
   currentPic: number
   Array = [
     { Ind: "1/1/6/4", photos: ["#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8"] },
-    { Ind: "1/4/6/7", photos: ["#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f"] },
-    { Ind: "1/7/9/11", photos: ["#79806e" ,"#61da5e" ,"#cd2f00"] },
-    { Ind: "1/11/9/15", photos: ["#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8"] },
-    { Ind: "1/15/7/21", photos: ["#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ] },
+    { Ind: "1/4/6/7", photos: ["#f205e6", "#1c0365", "#14a9ad", "#4ca2f9", "#a4e43f"] },
+    { Ind: "1/7/9/11", photos: ["#79806e", "#61da5e", "#cd2f00"] },
+    {
+      Ind: "1/11/9/15",
+      photos: ["#2f3f94", "#2f7b99", "#da967d", "#34891f", "#b0d87b", "#ca4751", "#7e50a8"],
+    },
+    { Ind: "1/15/7/21", photos: ["#c4d647", "#e0eeb8", "#11dec1", "#289812"] },
     { Ind: "6/1/14/7", photos: ["#05d371", "#5426e0", "#4834d0"] },
     { Ind: "9/7/19/12", photos: ["#615af0", "#21538e", "#89d534", "#d36647"] },
     { Ind: "9/12/14/15", photos: ["#ab4653", "#67eb4b", "#986b53"] },
@@ -29,16 +32,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
     { Ind: "14/1/19/4", photos: ["#c63b4e", "#983f7a", "#ea24a3"] },
     { Ind: "14/4/19/7", photos: ["#93f2d7", "#9b5c2a", "#15b9ee"] },
     { Ind: "13/15/19/21", photos: ["#df77b9", "#986b53", "#f50422", "#983f7a", "#ea24a3"] },
-    { Ind: "7/15/13/18", photos: ["#911e7e", "#3f16d9", "#0f525f", "#ac7c0a", "#b4c086", "#c9d730", "#30cc49"] },
+    {
+      Ind: "7/15/13/18",
+      photos: ["#911e7e", "#3f16d9", "#0f525f", "#ac7c0a", "#b4c086", "#c9d730", "#30cc49"],
+    },
     { Ind: "7/18/13/21", photos: ["#1350ce", "#10e5b1", "#fff4d7", "#cb2582", "#ce00be"] },
   ]
 
   constructor(private renderer: Renderer2) {}
 
   getRandomInt(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
   }
 
   RandomPhotoSlider(Num: number) {
@@ -51,26 +57,36 @@ export class HomeComponent implements OnInit, AfterViewInit {
         if (randomPhoto === this.currentPic) {
           null
         } else {
+          let height = parseInt(
+            this.linkRefs._results[randomPhoto].nativeElement.style.height.replace("%", "")
+          )
+          let width = parseInt(
+            this.linkRefs._results[randomPhoto].nativeElement.style.width.replace("%", "")
+          )
           let Dir = this.linkRefs._results[randomPhoto].nativeElement.style.transform.replace(
-            "translate",
+            "translate3d(",
             ""
           )
           const totalChildren = this.linkRefs._results[randomPhoto].nativeElement.children.length
-          let limit = Math.floor(100 - 100 / totalChildren) * -1
+          let limit = parseFloat((-100 + 100 / totalChildren).toFixed(2))
           let Proposed = ""
-          if (Dir[0] === "Y") {
-            let PresentY = parseInt(Dir.replace("Y(", "").replace("%)", ""))
+          if (height > width) {
+            let PresentY = parseFloat(Dir.replace("%,0px)", "").replace("0%,", ""))
             if (PresentY === limit) {
-              Proposed = `translateY(0%)`
+              Proposed = `translate3d(0%,0%,0px)`
             } else {
-              Proposed = `translateY(${PresentY - 100 / totalChildren}%)`
+              Proposed = `translate3d(0%,${parseFloat(
+                (PresentY - 100 / totalChildren).toFixed(2)
+              )}%,0px)`
             }
           } else {
-            let PresentX = parseInt(Dir.replace("X(", "").replace("%)", ""))
+            let PresentX = parseFloat(Dir.replace(",0%,0px)", "").replace("%", ""))
             if (PresentX === limit) {
-              Proposed = `translateX(0%)`
+              Proposed = `translate3d(0%,0%,0px)`
             } else {
-              Proposed = `translateX(${PresentX - 100 / totalChildren}%)`
+              Proposed = `translate3d(${parseFloat(
+                (PresentX - 100 / totalChildren).toFixed(2)
+              )}%,0%,0px)`
             }
           }
           this.renderer.setStyle(
@@ -103,30 +119,33 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.VisibleTimeout(0)
   }
-  
+
   QuickSlider() {
+    let height = parseInt(
+      this.linkRefs._results[this.currentPic].nativeElement.style.height.replace("%", "")
+    )
+    let width = parseInt(
+      this.linkRefs._results[this.currentPic].nativeElement.style.width.replace("%", "")
+    )
     let Dir = this.linkRefs._results[this.currentPic].nativeElement.style.transform.replace(
-      "translate",
+      "translate3d(",
       ""
     )
     const totalChildren = this.linkRefs._results[this.currentPic].nativeElement.children.length
-    let limit = Math.floor(100 - 100 / totalChildren) * -1
-    if (Dir[0] === "Y") {
-      this.renderer.setStyle(
-        this.linkRefs._results[this.currentPic].nativeElement,
-        "transform",
-        "translateY(0%)"
-      )
-    } else {
-      this.renderer.setStyle(
-        this.linkRefs._results[this.currentPic].nativeElement,
-        "transform",
-        "translateX(0%)"
-      )
-    }
+    let limit = parseFloat((-100 + 100 / totalChildren).toFixed(2))
+    this.renderer.setStyle(
+      this.linkRefs._results[this.currentPic].nativeElement,
+      "transform",
+      "translate3d(0%,0%,0px)"
+    )
+
     const quickTimeout = (Type: string, Limit: number, TC: number, currVal: number) => {
       setTimeout(() => {
-        let value = currVal - 100 / TC
+        /* 
+        this part of code has bug check the console for more detail
+        */
+
+        let value = parseFloat((currVal - 100 / TC).toFixed(2))
         console.log(limit, currVal)
         if (currVal <= Limit || this.currentPic === -1) {
           null
@@ -135,14 +154,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
             this.renderer.setStyle(
               this.linkRefs._results[this.currentPic].nativeElement,
               "transform",
-              `translateX(${value}%)`
+              `translate3d(${value}%,0%,0px)`
             )
           } else {
             if (this.currentPic >= 0) {
               this.renderer.setStyle(
                 this.linkRefs._results[this.currentPic].nativeElement,
                 "transform",
-                `translateY(${value}%)`
+                `translate3d(0%,${value}%,0px)`
               )
             }
           }
@@ -150,7 +169,11 @@ export class HomeComponent implements OnInit, AfterViewInit {
         }
       }, 1600)
     }
-    quickTimeout(Dir[0], limit, totalChildren, 0)
+    if (height > width) {
+      quickTimeout("Y", limit, totalChildren, 0)
+    } else {
+      quickTimeout("X", limit, totalChildren, 0)
+    }
   }
 
   MouseMoved(ID: number, stat: string) {
@@ -158,20 +181,11 @@ export class HomeComponent implements OnInit, AfterViewInit {
       this.currentPic = ID
       this.QuickSlider()
     } else {
-      let Dir = this.linkRefs._results[ID].nativeElement.style.transform.replace("translate", "")
-      if (Dir[0] === "Y") {
-        this.renderer.setStyle(
-          this.linkRefs._results[ID].nativeElement,
-          "transform",
-          "translateY(0%)"
-        )
-      } else {
-        this.renderer.setStyle(
-          this.linkRefs._results[ID].nativeElement,
-          "transform",
-          "translateX(0%)"
-        )
-      }
+      this.renderer.setStyle(
+        this.linkRefs._results[ID].nativeElement,
+        "transform",
+        "translate3d(0%,0%,0px)"
+      )
       this.currentPic = -1
     }
   }
