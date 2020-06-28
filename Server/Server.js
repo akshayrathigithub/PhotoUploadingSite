001fda353b7d21fa1b36822f5b418ba815581f52
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-28 14:07:21 +0530

    Basic Server Setup

--- Server/Server.js
+++ Server/Server.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+const Port = 3000 || process.env
+
+app.listen(Port, ()=>{ console.log("Server is running at "+ Port)})
