7c430534c3329ccf8095be92eed9699723aaee0b
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-28 11:58:17 +0530

    color overwritten bug in preview section fixed

--- src/app/components/photo-upload/photo-upload.component.ts
+++ src/app/components/photo-upload/photo-upload.component.ts
@@ -9,10 +9,27 @@ export class PhotoUploadComponent implements OnInit {
   FileDragged: boolean = false
   FileDropped: boolean = false
   DraggedPhoto: any
-  DraggedPhotoProperty: string;
+  DraggedPhotoProperty: string
   DroppedOn: any
-  DroppedOnProperty: string;
-  ColorArr = ["#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8","#1350ce", "#10e5b1", "#fff4d7", "#cb2582", "#ce00be", "#df77b9", "#986b53", "#f50422", "#983f7a", "#ea24a3"]
+  DroppedOnProperty: string
+  flag: boolean = true
+  ColorArr = [
+    "#63b598",
+    "#ce7d78",
+    "#ea9e70",
+    "#a48a9e",
+    "#c6e1e8",
+    "#1350ce",
+    "#10e5b1",
+    "#fff4d7",
+    "#cb2582",
+    "#ce00be",
+    "#df77b9",
+    "#986b53",
+    "#f50422",
+    "#983f7a",
+    "#ea24a3",
+  ]
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
@@ -42,23 +59,26 @@ export class PhotoUploadComponent implements OnInit {
   PhotoDropped(e: Event) {
     e.preventDefault()
     this.DroppedOn = e.target
-    if(this.DroppedOn != this.DraggedPhoto){
-      this.renderer.setStyle(this.DroppedOn, 'backgroundColor', this.DraggedPhotoProperty)
-      this.renderer.setStyle(this.DraggedPhoto, 'backgroundColor', this.DroppedOnProperty)
+    if (!this.flag && this.DroppedOn != this.DraggedPhoto) {
+      this.renderer.setStyle(this.DroppedOn, "backgroundColor", this.DraggedPhotoProperty)
+      this.renderer.setStyle(this.DraggedPhoto, "backgroundColor", this.DroppedOnProperty)
+      this.flag = true
     }
   }
   onPhotoDragEnter(e) {
-    if(e.target != this.DraggedPhoto){
+    if (this.flag && e.target != this.DraggedPhoto) {
       this.DroppedOn = e.target
       this.DroppedOnProperty = e.target.style.backgroundColor
-      this.renderer.setStyle(e.target, 'backgroundColor', this.DraggedPhotoProperty)
-      this.renderer.setStyle(this.DraggedPhoto, 'backgroundColor', this.DroppedOnProperty)
+      this.renderer.setStyle(e.target, "backgroundColor", this.DraggedPhotoProperty)
+      this.renderer.setStyle(this.DraggedPhoto, "backgroundColor", this.DroppedOnProperty)
+      this.flag = false
     }
   }
   onPhotoDragLeave(e: Event) {
-    if(e.target != this.DraggedPhoto){
-      this.renderer.setStyle(this.DroppedOn, 'backgroundColor', this.DroppedOnProperty)
-      this.renderer.setStyle(this.DraggedPhoto, 'backgroundColor', this.DraggedPhotoProperty)
+    if ( !this.flag && this.DroppedOn != this.DraggedPhoto) {
+      this.renderer.setStyle(this.DroppedOn, "backgroundColor", this.DroppedOnProperty)
+      this.renderer.setStyle(this.DraggedPhoto, "backgroundColor", this.DraggedPhotoProperty)
+      this.flag = true
     }
   }
 }
